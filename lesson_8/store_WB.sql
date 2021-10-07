75cabe28aab19f08d0e0810a5ab692c63f6a8190
shamma <shammacode@gmail.com>
2021-10-07 06:57:23 +0300

    lesson 8 task admin page

--- lesson_8/store_WB.sql
+++ lesson_8/store_WB.sql
@@ -30,7 +30,7 @@ CREATE TABLE `cart` (
   PRIMARY KEY (`id`),
   KEY `prod_id_from_cart_idx` (`prod_id`),
   CONSTRAINT `prod_id_from_cart` FOREIGN KEY (`prod_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=177 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=214 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `cart` (
 
 LOCK TABLES `cart` WRITE;
 /*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+INSERT INTO `cart` VALUES (184,1,1,'t83blffj2gim2lvgjn9h01cl8s3rtrog'),(201,1,1,'6lvnmaagjgjklc8kne9q2qu5fcssm9ls'),(202,2,1,'6lvnmaagjgjklc8kne9q2qu5fcssm9ls'),(203,2,1,'9na70lqftjkkekh4j39f31m9c6k4i1jn'),(204,1,1,'9na70lqftjkkekh4j39f31m9c6k4i1jn'),(205,1,2,'uqmdpqvs6baj5q6lof97p5rvoul6c0ui'),(206,1,2,'m3mjsqb9420noahevjasp3c4fpe2l9ki'),(207,1,1,'q5b6kpbkvc227g7e5ufgp4u1g82j2jkj'),(208,1,1,'lgotf3lc8cutji98vi2npum15bsc183o'),(209,1,1,'0fb9hbo1iiip2vgg4ci9v2iq1os7s9f9'),(210,2,1,'1cajsdn58g9euk7j3dpc2qj0onbc8pks'),(211,3,1,'1cajsdn58g9euk7j3dpc2qj0onbc8pks'),(212,1,1,'k7609mucjhnop49dok71gqgn3fs0fks6'),(213,2,1,'k7609mucjhnop49dok71gqgn3fs0fks6');
 /*!40000 ALTER TABLE `cart` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,6 +72,34 @@ INSERT INTO `comments` VALUES (1,'Вася','Стильно, модно, мол�
 UNLOCK TABLES;
 
 --
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_uid` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `total` decimal(20,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (6,'k7609mucjhnop49dok71gqgn3fs0fks6','12','122','2021-10-06 17:02:10',146.44);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `products`
 --
 
@@ -98,6 +127,58 @@ INSERT INTO `products` VALUES (1,'feature_1.png','Рубашка','Стильн�
 UNLOCK TABLES;
 
 --
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` tinyint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin'),(2,'client'),(3,'manager');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` tinyint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_from_role_tbl_idx` (`role`),
+  CONSTRAINT `role_from_role_tbl` FOREIGN KEY (`role`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','$2y$10$DMMzFPwxZNv4NO2ayLCsOOBIxmJLL3ro6ZLbAhRoQ5JqnHybNx8jy',1),(2,'user','$2y$10$IJa2aYy7Ca1v1aHjykjixu.m91qKnxZAwU9j/fkZNJJd5GGoCUBOG',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping events for database 'store'
 --
 
@@ -114,4 +195,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-03 13:14:26
+-- Dump completed on 2021-10-07  6:54:33
